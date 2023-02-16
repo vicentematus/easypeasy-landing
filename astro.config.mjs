5650fe9dfb5a141fbf225b2ef85733035c2e8068
Vicente <vicentematusortiz@gmail.com>
2023-02-16 00:38:33 -0300

    changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
 import compress from "astro-compress";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-   site: 'https://unwraped.design',
-  integrations: [tailwind(), image(), compress(), partytown(), sitemap()]
+	site: "https://unwraped.design",
+	integrations: [tailwind(), compress(), partytown(), sitemap()],
 });
